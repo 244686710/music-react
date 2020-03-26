b2755170bf6c8b54eef3cde7fb4414ffdbb9e530
244686710 <244686710@qq.com>
2020-03-26 17:16:33 +0800

    推荐列表开发

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Slider from '../../components/slider';
+import * as actionTypes from './store/actionCreators';
+import { connect } from "react-redux";
+import RecommendList from '../../components/list';
+import Scroll from '../../components/scroll';
+import { Content } from './style'
+import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/loading/index';
 
-function Recommend() {
+function Recommend(props) {
+  const { bannerList, recommendList, enterLoading } = props;
+  const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+  useEffect(() => {
+    // 如果页面有数据，则不发请求
+    // immutable 数据结构中长度属性 size
+    if (!bannerList.size) {
+      getBannerDataDispatch();      
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();      
+    }
+  }, [bannerList.size, getBannerDataDispatch, getRecommendListDataDispatch, recommendList.size]);
 
-    // mock数据
-    const bannerList = [1, 2, 3, 4].map(item => {
-        return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
-    });
+  const bannerListJS = bannerList ? bannerList.toJS() : [];
+  const recommendListJS = recommendList ? recommendList.toJS () :[];
 
-    return (
+  return (
+    <Content>
+      <div className="before"></div>
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
-            <Slider bannerList={bannerList}></Slider>  
+          <Slider bannerList={bannerListJS}></Slider>  
+          <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
-    )
+      </Scroll>
+
+      { enterLoading ?  <Loading></Loading> : null } 
+    </Content>
+  )
 }
 
-export default React.memo(Recommend)
+// 映射 Redux 全局的 state 到组件props上
+const mapStateToProps = (state) => ({
+    // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn (['recommend', 'bannerList']),
+  recommendList: state.getIn(['recommend', 'recommendList']),
+  enterLoading: state.getIn (['recommend', 'enterLoading'])
+})
+
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+    return {
+      getBannerDataDispatch () {
+        dispatch (actionTypes.getBannerList());
+      },
+      getRecommendListDataDispatch () {
+        dispatch (actionTypes.getRecommendList());
+      },
+    }
+  };
+// 将 ui 组件包装成容器组件
+export default connect (mapStateToProps, mapDispatchToProps)(React.memo (Recommend));
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -8,6 +8,7 @@ function Slider(props) {
     const { bannerList } = props;
     
     useEffect(() => {
+        // effect 的条件执行,  可以给 useEffect 传递第二个参数，它是 effect 所依赖的值数组
         if (bannerList.length && !sliderSwiper) {
             let sliderSwiper = new Swiper(".slider-container", {
                 loop: true,
@@ -23,12 +24,13 @@ function Slider(props) {
 
     return (
         <SliderContainer>
+            <div className="before"></div>
             <div className="slider-container">
                 <div className="swiper-wrapper">
                     {
-                        bannerList.map(slider => {
+                        bannerList.map((slider, index) => {
                             return (
-                                <div className="swiper-slide" key={slider.imageUrl}>
+                                <div className="swiper-slide" key={slider.imageUrl + index}>
                                     <div className="slider-nav">
                                         <img src={slider.imageUrl} width="100%" height="100%" alt="推荐"/>
                                     </div>
@@ -43,4 +45,5 @@ function Slider(props) {
     )
 }
 
+// React.memo 如果你的函数组件在给定相同 props 的情况下渲染相同的结果，那么你可以通过将其包装在 React.memo 中调用，以此通过记忆组件渲染结果的方式来提高组件的性能表现。这意味着在这种情况下，React 将跳过渲染组件的操作并直接复用最近一次渲染的结果。React.memo 仅检查 props 变更。如果函数组件被 React.memo 包裹，且其实现中拥有 useState 或 useContext 的 Hook，当 context 发生变化时，它仍会重新渲染。
 export default React.memo(Slider);
